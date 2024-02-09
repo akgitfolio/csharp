2a34ca00557cc6979cb91d494a927c448f0f4c99
ak <akv.git@gmail.com>
2024-02-09 22:38:00 +0000

    ft: udp multicast

---
+++ UDPMulticast/UDPMulticastClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MulticastClient
+{
+    private const string MulticastAddress = "239.0.0.1";
+    private const int MulticastPort = 8888;
+
+    static void Main(string[] args)
+    {
+        IPAddress multicastIP = IPAddress.Parse(MulticastAddress);
+        IPEndPoint localEP = new IPEndPoint(IPAddress.Any, MulticastPort);
+
+        using (UdpClient udpClient = new UdpClient())
+        {
+            udpClient.ExclusiveAddressUse = false;
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Client.Bind(localEP);
+            udpClient.JoinMulticastGroup(multicastIP);
+
+            Console.WriteLine("Listening for multicast messages...");
+
+            while (true)
+            {
+                IPEndPoint remoteEP = null;
+                byte[] data = udpClient.Receive(ref remoteEP);
+
+                // Decode binary data
+                string message = Encoding.UTF8.GetString(data);
+
+                Console.WriteLine($"Received: {message} from {remoteEP}");
+            }
+        }
+    }
+}
---
+++ UDPMulticast/UDPMulticastServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MulticastServer
+{
+    private const string MulticastAddress = "239.0.0.1";
+    private const int MulticastPort = 8888;
+
+    static void Main(string[] args)
+    {
+        IPAddress multicastIP = IPAddress.Parse(MulticastAddress);
+        IPEndPoint remoteEP = new IPEndPoint(multicastIP, MulticastPort);
+
+        using (UdpClient udpClient = new UdpClient())
+        {
+            udpClient.JoinMulticastGroup(multicastIP);
+
+            while (true)
+            {
+                Console.Write("Enter message to send: ");
+                string message = Console.ReadLine();
+
+                // Encode message as binary
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                // Send data
+                udpClient.Send(data, data.Length, remoteEP);
+
+                Console.WriteLine($"Sent: {message}");
+            }
+        }
+    }
+}
